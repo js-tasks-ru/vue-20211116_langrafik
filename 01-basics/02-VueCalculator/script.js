e5fd2e94a8e8344e19da1dafd539409740d98c69
Stanislav Zaicev <langrafik@gmail.com>
2021-11-27 15:15:13 +0300

    calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,37 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
+
+const Calculator = defineComponent({
+  data: () => ({
+    leftValue: 0,
+    rightValue: 0,
+    operation: '+',
+  }),
+  computed: {
+    result() {
+      switch (this.operation) {
+        case '+':
+          return Number(this.leftValue) + Number(this.rightValue);
+        case '-':
+          return Number(this.leftValue) - Number(this.rightValue);
+        case '*':
+          return Number(this.leftValue) * Number(this.rightValue);
+        case '/':
+          return Math.floor(Number(this.leftValue) / Number(this.rightValue));
+      }
+
+      return 0;
+    },
+  },
+  methods: {
+    changeOpertaion({target}) {
+      if (target.type === 'radio') {
+        const { name, value } = target;
+        this.operation = name;
+      }
+    }
+  }
+});
+
+createApp(Calculator).mount('#app');
 
 // Создайте Vue приложение
