c6e5cd50911224b555b8c528a74880bfdb69a6c5
Stanislav Zaicev <langrafik@gmail.com>
2021-11-27 15:48:41 +0300

    email filter

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,19 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+const Calculator = defineComponent({
+  data: () => ({
+    inputValue: '',
+  }),
+  computed: {
+    filteredList() {
+      return emails.map((email) => ({
+        title: email,
+        class: this.inputValue ? email.toLowerCase().includes(this.inputValue.toLowerCase()) ? 'marked' : '' : '',
+      }));
+    },
+  },
+});
+
+
+createApp(Calculator).mount('#app');
